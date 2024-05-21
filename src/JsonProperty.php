d49a53604ed7228f50fd82873ba111bdb60ebf3f
JuraSciix <jurasciix@gmail.com>
2024-05-21 23:52:43 +0300

    doc: уточняет документацию к JsonProperty.php

--- src/JsonProperty.php
+++ src/JsonProperty.php
@@ -5,6 +5,12 @@ namespace jurasciix\objeckson;
 use Attribute;
 use InvalidArgumentException;
 
+/**
+ * Указывает, что поле или класс являются частью модели JSON.
+ *
+ * JsonProperty можно повесить на свойство класса или на сам класс.
+ * Если повесить JsonProperty на класс, то он будет относиться к каждому свойству класса.
+ */
 #[Attribute(Attribute::TARGET_PROPERTY | Attribute::TARGET_CLASS_CONSTANT | Attribute::TARGET_CLASS)]
 class JsonProperty {
 
